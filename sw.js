816da644fc54f65320d939d57b52986317a6d2ec
Brad38 <brad.gui22@gmail.com>
2026-09-11 23:19:59 -0300

    Adicionando novo icone do site

--- sw.js
+++ sw.js
@@ -5,6 +5,7 @@ const ASSETS = [
   './styles.css',
   './app.js',
   './manifest.webmanifest',
+  './icons/icon.svg',
   './icons/icon-192.png',
   './icons/icon-512.png',
   './icons/apple-touch-icon.png',
